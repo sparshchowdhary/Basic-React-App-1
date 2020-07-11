32410e92cee5bdcacaebd9e10c3ef672dd46a36e
Sparsh Chowdhary <sparshchoudhary@gmail.com>
2020-07-11 22:55:36 +0530

    Build files for GitHub pages

---
+++ build/precache-manifest.4dbb609937efd8422fc9cb0eea7b7f43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbd2334ab627db8c18543c76120c993",
+    "url": "/Basic-React-App-1/index.html"
+  },
+  {
+    "revision": "bc37bfda824bfc35e7e4",
+    "url": "/Basic-React-App-1/static/css/main.d890d556.chunk.css"
+  },
+  {
+    "revision": "a51b30ad1a8e592d51b5",
+    "url": "/Basic-React-App-1/static/js/2.80cc67e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Basic-React-App-1/static/js/2.80cc67e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc37bfda824bfc35e7e4",
+    "url": "/Basic-React-App-1/static/js/main.289e8621.chunk.js"
+  },
+  {
+    "revision": "d1420a27201f6765c59e",
+    "url": "/Basic-React-App-1/static/js/runtime-main.23ce4737.js"
+  }
+]);
